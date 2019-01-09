402f82eef2592a4b7aa35939d4e69e6a849a04cd
Brandon Kesselly <brandon.kesselly@hotmail.com>
2019-01-09 15:12:36 -0500

    Added images and updated styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: `rebeccapurple`,
+      background: `#f9ce12`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#906c00`,
             textDecoration: `none`,
           }}
         >
